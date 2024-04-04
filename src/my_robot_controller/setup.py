9fb2de5ee78af5e1f9cf9d96b78095d2f815c182
Liis Rossner <liisrossner01@gmail.com>
2024-04-04 10:44:20 +0300

    Homework 2

--- src/my_robot_controller/setup.py
+++ src/my_robot_controller/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_robot_controller'
 
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/maps', glob('maps/*')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -22,7 +26,8 @@ setup(
         'console_scripts': [
             "test_node = my_robot_controller.my_first_node:main",
             "draw_circle = my_robot_controller.draw_circle:main",
-            "mapping = my_robot_controller.mapping:main"
+            "mapping = my_robot_controller.mapping:main",
+            "navigation = my_robot_controller.navigation:main"
         ],
     },
 )
